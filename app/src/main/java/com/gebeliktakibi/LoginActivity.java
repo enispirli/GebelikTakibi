04cb9df78cd0d6c0ce5b66b677e602dabf2abd30
elifnur <en.ispirli@gmail.com>
2017-04-19 11:22:03 +0300

    Uygulamanın kayıt olma işlemi gerçekleştirildi

---
+++ app/src/main/java/com/gebeliktakibi/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.gebeliktakibi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/gebeliktakibi/MainActivity.java
@@ -0,0 +1,95 @@
+package com.gebeliktakibi;
+
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button buttonRegister;
+    private EditText editTextEmail;
+    private  EditText editTextPassword;
+    private TextView textViewSignin;
+
+    private ProgressDialog progressDialog;
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        firebaseAuth=FirebaseAuth.getInstance();
+
+        progressDialog=new ProgressDialog(this);
+
+        buttonRegister=(Button)findViewById(R.id.buttonRegister);
+        editTextEmail=(EditText)findViewById(R.id.editTextMail);
+        editTextPassword=(EditText)findViewById(R.id.editTextPassword);
+        textViewSignin=(TextView)findViewById(R.id.textViewSignin);
+
+        buttonRegister.setOnClickListener(this);
+        textViewSignin.setOnClickListener(this);
+
+    }
+
+    private void registerUser(){
+        String email=editTextEmail.getText().toString().trim();
+        String password=editTextPassword.getText().toString().trim();
+
+        if(TextUtils.isEmpty(email)){
+            //email is empty
+            Toast.makeText(this,"Lütfen email adresinizi giriniz",Toast.LENGTH_SHORT).show();
+            //stopping the function execution further
+            return;
+        }
+        if(TextUtils.isEmpty(password)){
+            //password is empty
+            Toast.makeText(this,"Lütfen şifrenizi giriniz",Toast.LENGTH_SHORT).show();
+            //stopping the function execution further
+            return;
+        }
+
+        progressDialog.setMessage("Bebek yükleniyor... :)");
+        progressDialog.show();
+
+        firebaseAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(MainActivity.this,"Kayıt başarılı anne adayımız hoşgeldiniz :)",Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(MainActivity.this,"Kayıt olamadınız, tekrar deneyin :(",Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==buttonRegister){
+            registerUser();
+        }
+        if(v==textViewSignin){
+            //...
+        }
+
+    }
+}
